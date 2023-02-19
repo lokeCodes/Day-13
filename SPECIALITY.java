7c9edb0998e211dc61d7f32b27d4bfda9a3e574b
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-19 21:56:27 +0530

    Add files via upload

---
+++ SPECIALITY.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-- > 0){
+		    int x = s.nextInt();
+		    int y = s.nextInt();
+		    int z = s.nextInt();
+		    int res = Math.max(x,Math.max(y,z));
+		    if(x==res){
+		        System.out.println("Setter");
+		    }else if(y==res){
+		        System.out.println("Tester");
+		    }else{
+		        System.out.println("Editorialist");
+		    }
+		}
+	}
+}
